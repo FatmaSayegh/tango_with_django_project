dc4437e7142b4e3b7eb0968534071c8c7f0645f2
Fatma Al Sayegh <2515006A@student.gla.ac.uk>
2021-01-28 20:56:23 +0000

    About Excercise git push asdf

--- rango/views.py
+++ rango/views.py
@@ -9,4 +9,7 @@ from django.http import HttpResponse
 
 # request is an HttpRequest
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
