6e7e2d9dee0a1cefc7f3f81e1114f0f80fe75b30
Fatma Al Sayegh <2515006A@student.gla.ac.uk>
2021-01-28 21:18:54 +0000

    First Template Implementation

--- rango/views.py
+++ rango/views.py
@@ -9,7 +9,9 @@ from django.http import HttpResponse
 
 # request is an HttpRequest
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    # all replacements
+    context_dict = {'boldmessage':  'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
