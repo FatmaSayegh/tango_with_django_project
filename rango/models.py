d901df4da80a61579a2de9df81cf6dc49ac3ed72
Fatma Al Sayegh <2515006A@student.gla.ac.uk>
2021-02-01 16:23:29 +0000

    Added Database and ran shell Test

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+# this is a table called category that has a name as a key, it is unique
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+# this is a table called Page, 
+# category is a foreign key
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
