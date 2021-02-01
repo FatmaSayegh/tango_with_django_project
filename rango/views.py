eec11b4545191add3514ffa5ccdc470b6ad4118c
Fatma Al Sayegh <2515006A@student.gla.ac.uk>
2021-02-01 17:49:45 +0000

    Ch.6 Category Slug

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-
+from rango.models import Category
 
 # each view returns an http response object
 # after creating a view, map it using urls.py, where each url 
@@ -9,8 +9,17 @@ from django.http import HttpResponse
 
 # request is an HttpRequest
 def index(request):
+    # load some stuff from the database
+    
+    # order in descending order, this the '-' begind likes
+    # limit to 5, thus the [:5]
+    category_list = Category.objects.order_by('-likes')[:5]
+    
     # all replacements
-    context_dict = {'boldmessage':  'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
